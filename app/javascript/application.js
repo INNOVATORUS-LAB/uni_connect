67d7f81809ca334956abe0a81e8f4ae50e709029
Chris Siku <chrissiku5@gmail.com>
2023-04-27 16:57:01 +0200

    Setup Javascript frontend

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,11 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
+// import "@hotwired/turbo-rails";
+// import "controllers";
+
+const body = document.getElementById("home");
+
+document.addEventListener("DOMContentLoaded", function () {
+  let greeting = document.createElement("p");
+  greeting.textContent = "Hello, World!";
+  document.body.appendChild(greeting);
+});
